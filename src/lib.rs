09f0d7c2ccf550ff061ad1f5ca32aec2d017a202
E-gy <balakhelisey@gmail.com>
2021-05-27 16:07:08 +0200

    GeoJSON

--- src/lib.rs
+++ src/lib.rs
@@ -492,6 +492,29 @@ extern "C" {
     #[wasm_bindgen(method)]
     pub fn getBounds(this: &FeatureGroup) -> LatLngBounds;
 
+    // GeoJSON
+
+    /// [`GeoJSON`](https://leafletjs.com/reference-1.7.1.html#geojson)
+    #[derive(Clone, Debug)]
+    #[wasm_bindgen(extends = Layer)]
+    pub type GeoJSON;
+
+    /// [`L.geoJSON`](https://leafletjs.com/reference-1.7.1.html#geojson-l-geojson)
+    #[wasm_bindgen(js_namespace = L)]
+    pub fn geoJSON(geojson: &JsValue, options: &JsValue) -> GeoJSON;
+
+    /// [`addData`](https://leafletjs.com/reference-1.7.1.html#geojson-adddata)
+    #[wasm_bindgen(method)]
+    pub fn addData(this: &GeoJSON, data: &JsValue);
+
+    /// [`resetStyle`](https://leafletjs.com/reference-1.7.1.html#geojson-resetstyle)
+    #[wasm_bindgen(method)]
+    pub fn resetStyle(this: &GeoJSON, layer: Option<&Layer>);
+
+    /// [`setStyle`](https://leafletjs.com/reference-1.7.1.html#geojson-setstyle)
+    #[wasm_bindgen(method)]
+    pub fn setStyle(this: &GeoJSON, style: &JsValue);
+
     // TileLayer
 
     #[derive(Debug)]
