a05060dfff6f4dadfa99e4d08355798ce52154d4
Manfred Kroehnert <7868+mkroehnert@users.noreply.github.com>
2021-06-29 22:05:58 +0200

    PolyLine derives from Path, Circle derives from CircleMarker

--- src/lib.rs
+++ src/lib.rs
@@ -423,8 +423,9 @@ extern "C" {
 
     // Polyline
 
+    /// [`Polyline`](https://leafletjs.com/reference-1.7.1.html#polyline)
     #[derive(Debug)]
-    #[wasm_bindgen(extends = Layer)]
+    #[wasm_bindgen(extends = Path)]
     pub type Polyline;
 
     #[wasm_bindgen(constructor, js_namespace = L)]
@@ -495,7 +496,7 @@ extern "C" {
     // Circle
 
     #[derive(Debug)]
-    #[wasm_bindgen(extends = Layer)]
+    #[wasm_bindgen(extends = CircleMarker)]
     pub type Circle;
 
     #[wasm_bindgen(constructor, js_namespace = L)]
@@ -503,6 +504,14 @@ extern "C" {
 
     #[wasm_bindgen(constructor, js_namespace = L)]
     pub fn new_with_options(latlng: &LatLng, options: &JsValue) -> Circle;
+
+    /// [`setRadius`](https://leafletjs.com/reference-1.7.1.html#circle-setradius)
+    #[wasm_bindgen(method)]
+    pub fn setRadius(this: &Circle, radius: f64);
+
+    /// [`getRadius`](https://leafletjs.com/reference-1.7.1.html#circle-getradius)
+    #[wasm_bindgen(method)]
+    pub fn getRadius(this: &Circle) -> f64;
 
     // FeatureGroup
 
