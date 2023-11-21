a315d79cc76c7cd32d5aea0ac96c8ee64df4d531
Lewin Probst <info@emirror.de>
2023-11-21 16:10:20 +0100

    refactor: Replaced get_feature_info_url by WmsRequestBuilder that provides more flexibility

--- src/raster/tile_layer_wms.rs
+++ src/raster/tile_layer_wms.rs
@@ -1,4 +1,5 @@
 use js_sys::Object;
+use url::{ParseError, Url};
 use wasm_bindgen::prelude::*;
 
 use crate::{create_object_with_properties, Crs, LatLng, Map, TileLayer, TileLayerOptions};
@@ -43,47 +44,6 @@ impl TileLayerWms {
             .expect("Property wmsParams not available.")
             .into()
     }
-
-    /// Creates a feature info URL that can be used to query a feature from a WMS service.
-    /// Based on [BetterWMS](https://github.com/sazal-ns/BetterWMS).
-    #[must_use]
-    pub fn get_feature_info_url(&self, map: &Map, latlng: &LatLng) -> String {
-        let wms_params = self.get_params();
-        let map_size = map.get_size();
-        let point = map.lat_lng_to_container_point(latlng);
-
-        let wms_request_parameter = WmsRequestParameter::new();
-        wms_request_parameter.set_request("GetFeatureInfo".to_string());
-        wms_request_parameter.set_service("WMS".to_string());
-        wms_request_parameter.set_srs("EPSG:4326".to_string());
-        wms_request_parameter.set_styles(wms_params.styles());
-        wms_request_parameter.set_transparent(wms_params.transparent());
-        wms_request_parameter.set_version(wms_params.version());
-        wms_request_parameter.set_format(wms_params.format());
-        wms_request_parameter.set_bbox(map.get_bounds().to_bbox_string());
-        wms_request_parameter.set_height(map_size.y());
-        wms_request_parameter.set_width(map_size.x());
-        wms_request_parameter.set_layers(wms_params.layers());
-        wms_request_parameter.set_query_layers(wms_params.layers());
-        wms_request_parameter.set_info_format("geojson".to_string());
-
-        let x = point.x().floor();
-        let y = point.y().floor();
-
-        if &wms_params.version()[..] == "1.3.0" {
-            wms_request_parameter.set_i(x);
-            wms_request_parameter.set_j(y);
-        } else {
-            wms_request_parameter.set_x(x);
-            wms_request_parameter.set_y(y);
-        }
-        self.url()
-            + &crate::Util::get_param_string_url_uppercase(
-                wms_request_parameter.into(),
-                &self.url(),
-                true,
-            )
-    }
 }
 
 create_object_with_properties!(
@@ -123,3 +83,93 @@ create_object_with_properties!(
     (i, i, f64),
     (j, j, f64)
 );
+
+/// Construct a request URL for a WMS service.
+///
+/// # Defaults
+/// The default implementation creates a request with the following adjusted parameters:
+///
+/// - Request: `GetFeatureInfo`
+/// - Service: `WMS`
+/// - SRS: `EPSG:3426`
+/// - Info format: `geojson`
+pub struct WmsRequestBuilder {
+    params: WmsRequestParameter,
+}
+
+impl WmsRequestBuilder {
+    /// Sets the service parameter.
+    pub fn with_service(self, service: &str) -> Self {
+        self.params.set_service(service.to_string());
+        self
+    }
+
+    /// Sets the request parameter.
+    pub fn with_request(self, request: &str) -> Self {
+        self.params.set_request(request.to_string());
+        self
+    }
+
+    /// Sets the srs parameter.
+    pub fn with_srs(self, srs: &str) -> Self {
+        self.params.set_srs(srs.to_string());
+        self
+    }
+
+    /// Sets the info format parameter.
+    pub fn with_info_format(self, info_format: &str) -> Self {
+        self.params.set_info_format(info_format.to_string());
+        self
+    }
+
+    /// Creates the [Url] for the request with the parameters set previously.
+    pub fn build(
+        &self,
+        map: &Map,
+        layer: &TileLayerWms,
+        latlng: &LatLng,
+    ) -> Result<Url, ParseError> {
+        let wms_params = layer.get_params();
+        let map_size = map.get_size();
+        let point = map.lat_lng_to_container_point(latlng);
+
+        self.params.set_styles(wms_params.styles());
+        self.params.set_transparent(wms_params.transparent());
+        self.params.set_version(wms_params.version());
+        self.params.set_format(wms_params.format());
+        self.params.set_bbox(map.get_bounds().to_bbox_string());
+        self.params.set_height(map_size.y());
+        self.params.set_width(map_size.x());
+        self.params.set_layers(wms_params.layers());
+        self.params.set_query_layers(wms_params.layers());
+        match &wms_params.version()[..] {
+            "1.3.0" => {
+                self.params.set_i(point.x().floor());
+                self.params.set_j(point.y().floor());
+            }
+            _ => {
+                self.params.set_x(point.x().floor());
+                self.params.set_y(point.y().floor());
+            }
+        }
+        Url::parse(
+            &(layer.url()
+                + &crate::Util::get_param_string_url_uppercase(
+                    self.params.clone().into(),
+                    &layer.url(),
+                    true,
+                )),
+        )
+    }
+}
+
+impl Default for WmsRequestBuilder {
+    fn default() -> Self {
+        let params = WmsRequestParameter::new();
+        params.set_request("GetFeatureInfo".to_string());
+        params.set_service("WMS".to_string());
+        params.set_srs("EPSG:4326".to_string());
+        params.set_info_format("geojson".to_string());
+        Self { params }
+    }
+}
