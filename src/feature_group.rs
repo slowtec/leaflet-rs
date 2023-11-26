4a4a4a4cc8668a5d3ab24cf6c60224d22b9afbf3
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-11-26 16:48:46 +0000

    fix: add missing FeatureGroup constructor

--- src/feature_group.rs
+++ src/feature_group.rs
@@ -9,6 +9,9 @@ extern "C" {
     #[wasm_bindgen(extends = LayerGroup)]
     pub type FeatureGroup;
 
+    #[wasm_bindgen(constructor, js_namespace = L)]
+    pub fn new() -> FeatureGroup;
+
     /// [`setStyle`](https://leafletjs.com/reference-1.7.1.html#featuregroup-setstyle)
     #[wasm_bindgen(method, js_name = "setStyle")]
     pub fn set_style(this: &FeatureGroup, style: &JsValue);
