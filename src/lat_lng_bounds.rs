f288c381fcc9255a50af9fc758dd6a22a9fd214b
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2025-09-19 08:40:22 +0200

    Add new builder for LatLngBounds

--- src/lat_lng_bounds.rs
+++ src/lat_lng_bounds.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-use crate::LatLng;
+use crate::{Array, LatLng};
 
 #[wasm_bindgen]
 extern "C" {
@@ -10,8 +10,23 @@ extern "C" {
     #[wasm_bindgen(constructor, js_namespace = L)]
     pub fn new(corner1: &LatLng, corner2: &LatLng) -> LatLngBounds;
 
+    #[wasm_bindgen(constructor, js_namespace = L)]
+    pub fn new_from_list(positions: &Array) -> LatLngBounds;
+
+    #[wasm_bindgen(method, js_namespace = L)]
+    pub fn extend(this: &LatLngBounds, latlng: &LatLng) -> LatLngBounds;
+
+    #[wasm_bindgen(method, js_namespace = L, js_name = extend)]
+    pub fn extend_with_bounds(this: &LatLngBounds, bounds: &LatLngBounds) -> LatLngBounds;
+
     #[wasm_bindgen(method, js_name = getNorthEast)]
     pub fn get_north_east(this: &LatLngBounds) -> LatLng;
+
+    #[wasm_bindgen(method, js_name = getNorthWest)]
+    pub fn get_north_west(this: &LatLngBounds) -> LatLng;
+
+    #[wasm_bindgen(method, js_name = getSouthEast)]
+    pub fn get_south_east(this: &LatLngBounds) -> LatLng;
 
     #[wasm_bindgen(method, js_name = getSouthWest)]
     pub fn get_south_west(this: &LatLngBounds) -> LatLng;
